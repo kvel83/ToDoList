7d66eb67398a6fd68ae3c7d7811310b36ea4fb63
Katia Velasquez <katia.velasquez@.com>
2022-12-26 22:18:56 -0300

    subida de archivos

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+let tasks = [];
+const localStorageSave = () => {
+    if (!localStorage.getItem("tasksLS")){
+        localStorage.setItem("tasksLS" , JSON.stringify(tasks));
+    }else{
+        localStorage.removeItem("tasksLS");
+        localStorage.setItem("tasksLS" , JSON.stringify(tasks));
+    }
+};
+
+const addTask = () => {
+    let taskObj = {
+        id : 0,
+        task : "",
+        status : true //TRUE = TAREA PENDIENTE;FALSE = TAREA REALIZADA
+    }
+    let task = document.querySelector('#newTask').value;
+    document.querySelector('#newTask').value = "";
+    (tasks.length === 0)? taskObj.id = 1 : taskObj.id = tasks.length + 1;
+    taskObj.task = task;
+    tasks.push(taskObj);
+    localStorageSave();
+    printTasks();
+}
+
+const printTasks = () => {
+    let tasksList = ``;
+    let countTasks = 0;
+    let okTasks = 0;
+    tasks.forEach(task =>{
+        countTasks++;
+        tasksList += `
+            <div class="tarea">
+                <div class="idTask">
+                    <p>${task.id}</p>
+                    <p id="bodyTask">${task.task}</p>
+                </div>
+                <div class="taskActions">
+                    <span><i class="fa-solid fa-thumbs-up fa-2xl" id = "ok${task.id}" onclick = "doneTask(${task.id})"></i></span>
+                    <span><i class="fa-solid fa-trash fa-2xl" id ="del${task.id}" onclick = "deleteTask(${task.id})"></i></span>
+                </div>
+            </div>
+        `;
+        if(!task.status){
+            okTasks++;
+        }
+    })
+    document.querySelector('.tareas').innerHTML = tasksList;
+    document.querySelector('#totalTask').textContent = countTasks;
+    document.querySelector('#okTask').textContent = okTasks;
+    paintDone();
+}
+const paintDone = () => {
+    tasks.forEach(task =>{
+        if(task.status === false){
+            document.querySelector('#ok'+task.id).closest(".tarea").style.background = '#2f234b';
+            document.querySelector('#ok'+task.id).closest(".tarea").style.color = '#e0cbdc';
+            document.querySelector('#ok'+task.id).style.color = '#f74a8a';
+        }}
+    )
+}
+const deleteTask = (taskId) => {
+    const index = tasks.findIndex(task => task.id === taskId);
+    const modal = document.querySelector('#myModal')
+    modal.style.display = 'block';
+    modal.querySelector('#cancel').addEventListener('click',event => (modal.style.display = 'none'));
+    console.log(tasks);
+    modal.querySelector('#deleteTask').addEventListener('click',() => {
+        console.log(tasks);
+        tasks.splice(index,1);
+        console.log(tasks);
+        localStorageSave();
+        modal.style.display = 'none';
+    });
+    printTasks();
+}
+// const updateArray = (taskId) => {
+    // tasks.splice(taskId,1);
+    // localStorageSave();
+    // document.querySelector('#myModal').style.display = 'none';
+// }
+const doneTask = (taskId) =>{
+    const index = tasks.findIndex(task => task.id === taskId);
+    tasks[index].status = false;
+    localStorageSave();
+    printTasks();
+}
+const newTaskBtn = document.querySelector('#addTask');
+const newTaskInput = document.querySelector('#newTask');
+newTaskBtn.addEventListener('click',addTask);
+newTaskInput.addEventListener('keypress', event =>(event.keyCode === 13)?addTask():false)
+if(localStorage.getItem("tasksLS")){
+    tasks = JSON.parse(localStorage.getItem("tasksLS"));
+    printTasks();
+};
